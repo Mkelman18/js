8947af07c3260b45d531fadcf7338c6bce1e2a35
Mkelman18 <160253161+Mkelman18@users.noreply.github.com>
2025-02-28 11:08:12 -0300

    Add files via upload

---
+++ java/TiposPrimitivos.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+
+public class TiposPrimitivos {
+
+  public static void main(String args[]){
+
+    Scanner sr = new Scanner (System.in);
+  
+    boolean lampada = false;
+
+    char letra = 'M';
+
+    char primeiraletra = 'A';
+    char segundaletra = 'm';
+    char terceiraletra = 'o';
+    char quartaletra = 'r';
+
+    // System.out.print(primeiraletra);
+    // System.out.print(segundaletra);
+    // System.out.print(terceiraletra);
+    // System.out.print(quartaletra);
+
+    System.out.println( " " + primeiraletra + 
+    segundaletra + 
+    terceiraletra + 
+    quartaletra);
+
+     int numberoDaCasa = 1234;
+     float nomeroDoBingo = 3.40282347e+38f;
+
+    //  double numeroDeVisualizacao = 1.79769313486231570e+308;
+
+     double numeroDeVisualizacao = 1.79769313486231570e+308;
+
+     long numeroGrande = 922337203685477807L;
+     byte  idadeDaPessoa = 127;
+     short numeroCurto = 32767;
+
+    System.out.println(numberoDaCasa);
+    System.out.println(nomeroDoBingo);
+    System.out.println(numeroDeVisualizacao);
+    System.out.println(numeroGrande);
+    System.out.println(idadeDaPessoa);
+    System.out.println(numeroCurto);
+
+
+
+
+
+}
+
+  }    
